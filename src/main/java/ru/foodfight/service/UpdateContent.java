ae24390e22e258861f904b1e5a7ced351321e7c9
polyakamorph <polyakamorph@gmail.com>
2020-03-24 13:51:23 +0300

    Service to update content

---
+++ src/main/java/ru/foodfight/service/UpdateContent.java
@@ -0,0 +1,9 @@
+package ru.foodfight.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UpdateContent {
+    /* POST /updatecontent/ JSON here */
+    private String JSONstring;
+}
